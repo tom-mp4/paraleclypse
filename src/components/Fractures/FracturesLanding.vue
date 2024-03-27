820b56ec77f6a5496eaa4298be9761b1d57fee59
Tom <martin07tom@gmail.com>
2024-03-28 00:41:45 +0100

    Added new visuals assets for Fractures.vue Created FracturesLanding.vue Created Programmation.vue Added new font "Kanit"

--- src/components/Fractures/FracturesLanding.vue
+++ src/components/Fractures/FracturesLanding.vue
@@ -1,13 +1,36 @@
 <script setup>
-
 </script>
 
 <template>
   <div class="fracturesLanding">
-
+      <h1>fractures</h1>
+      <p>Les séances paraleclypse qui vous font découvrir les court-métrages au cinéma.</p>
   </div>
 </template>
 
 <style scoped lang="scss">
+
+.fracturesLanding {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  color: white;
+
+  h1 {
+    font-size: 9rem;
+    font-style: italic;
+    font-weight: 800;
+    font-family: Kanit, sans-serif, italic;
+    line-height: 1;
+    text-transform: uppercase;
+  }
+
+  p {
+    font-size: 1.3rem;
+    text-transform: uppercase;
+  }
+}
 
 </style>
--- src/views/Fractures.vue
+++ src/views/Fractures.vue
@@ -1,18 +1,36 @@
 <script setup>
 
-import WhiteNavbar from '@/components/WhiteNavbar.vue';
 import FracturesLanding from '@/components/Fractures/FracturesLanding.vue';
+import Programmation from '@/components/Fractures/Programmation.vue';
+import Navbar from '@/components/Navbar.vue';
 </script>
 
 <template>
   <div class="fractures">
-    <WhiteNavbar />
-    <div class="greyLine"></div>
-    <FracturesLanding />
+    <div class="fracturesBackground">
+      <Navbar />
+      <div class="line"></div>
+      <FracturesLanding />
+    </div>
+    <Programmation />
   </div>
 
 </template>
 
 <style scoped lang="scss">
+
+.fracturesBackground {
+  background-image: url("../assets/img/fractures-background.jpeg");
+  background-size: cover;
+  background-position: center;
+
+  .navbar {
+    background-color: transparent;
+  }
+
+  .line {
+    width: 100%;
+  }
+}
 
 </style>
