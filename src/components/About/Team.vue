e7d14f449414c8bede23d400faaaaa10e3528c83
Tom <martin07tom@gmail.com>
2024-10-07 15:16:48 +0200

    Created Team.vue section into About.vue page

---
+++ src/components/About/Team.vue
@@ -0,0 +1,24 @@
+<script setup>
+</script>
+
+<template>
+<section class="team">
+  <h2 class="title">
+    L'équipe
+  </h2>
+</section>
+</template>
+
+<style scoped lang="scss">
+
+.team{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background-color: #f5f5f5;
+  color: #000;
+  font-family: 'Syne', sans-serif;
+}
+
+</style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -8,6 +8,7 @@ import GraphicalCharter from '@/components/About/GraphicalCharter.vue';
 import CinemaAdvantages from '@/components/About/CinemaAdvantages.vue';
 import PublicAdvantages from '@/components/About/PublicAdvantages.vue';
 import RightsownerAdvantages from '@/components/About/RightsownerAdvantages.vue';
+// import Team from '@/components/About/Team.vue';
 </script>
 
 <template>
@@ -27,6 +28,8 @@ import RightsownerAdvantages from '@/components/About/RightsownerAdvantages.vue'
     <CinemaAdvantages />
     <div id="divider" class="greyLine"></div>
     <GraphicalCharter />
+    <!--<div id="divider" class="greyLine"></div>
+    <Team />-->
     <Footer />
   </div>
 </template>
