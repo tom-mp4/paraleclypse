d6265b13ea6ce4d6d7453fd5f9d5ec2b3bfcdbc1
Tom <martin07tom@gmail.com>
2025-06-14 18:21:17 +0200

    Edited text in Home.vue Started to change the Landing Page

---
+++ src/components/Home/VideoLanding.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="presentingVideo">
+    <video autoplay muted loop playsinline>
+      <source src="@/assets/video/trailer2.mp4" type="video/mp4">
+    </video>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped lang="scss">
+
+.presentingVideo {
+  position: relative;
+  width: 100%;
+  height: 100vh;
+  overflow: hidden;
+
+  video {
+    object-fit: cover;
+    object-position: center;
+    width: 100%;
+    height: 100%;
+
+  }
+}
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -12,9 +12,6 @@
     </div>
     <Menu />
     <CinemaMap />
-    <CompanyDescription
-      :content="build[1].content"/>
-    <Project />
     <Footer />
   </div>
 </template>
@@ -22,7 +19,6 @@
 <script>
 import Landing from '@/components/Home/Landing.vue';
 import CompanyDescription from '@/components/Home/CompanyDescription.vue';
-import Project from '@/components/Home/Project.vue';
 import Menu from '@/components/Home/Menu.vue';
 import CinemaMap from '@/components/Home/CinemaMap.vue';
 import Footer from '@/components/Footer.vue';
@@ -42,23 +38,18 @@ export default {
     Footer,
     CinemaMap,
     Menu,
-    Project,
     CompanyDescription,
     Landing,
   },
   data: () => ({
     build: [
       {
-        content: 'Paraleclypse est un studio de distribution indépendant dédié au court-métrage.'
-          + ' Notre volonté est de connecter un cinéma jeune et émergent avec un public curieux et habitué aux formats courts.'
-          + ' Nous proposons ainsi des séances de 75 à 90 minutes, composées de plusieurs courts-métrages enchaînés,'
-          + ' créant une expérience cinématographique riche et variée.',
-      },
-      {
-        content: 'Notre objectif est d\'accompagner les artistes pour lesquels nous\n'
-          + 'croyons dans le développement de leur projet. De l\'expertise technique et artistique, en passant par l\'évaluation\n'
-          + 'des coûts, la production et la diffusion, nous travaillons collectivement pour donner vie à de beaux projets.\n'
-          + 'Avant tout, nous souhaitons raconter une histoire, transmettre une émotion, et faire vivre une expérience.',
+        content: 'Paraleclypse est un studio de distribution indépendant de films de court-métrage.'
+          + ' La société distribue un catalogue de court-métrages de tous genres.'
+          + ' Fiction, documentaire, animation, expérimental, films de fin d\'études,'
+          + ' notre objectif est de faire vivre ces œuvres aux travers des festivals et des diffuseurs internationaux.'
+          + ' Depuis 2024, Paraleclypse distribue aussi son catalogue en salle de cinéma, au travers de programmes thématique ;'
+          + ' les séances Fractures.',
       },
     ],
   }),
